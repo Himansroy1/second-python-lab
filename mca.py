b763e1b2a77f563bcf5ba1f0308cbbb3d73467f7
Himanshu Kumar <hk7278144@gmail.com>
2026-09-10 20:22:56 +0530

    Initial commit

---
+++ mca.py
@@ -0,0 +1,5 @@
+print("Hello, World! from Graphic Era University")
+
+print("This is a sample Python script for demonstration purposes.")
+
+print("Himanshu Kumar\t MCA(G2)\t Roll No: 36\t Graphic Era University")
